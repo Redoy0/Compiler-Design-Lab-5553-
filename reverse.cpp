36b820c9637b3538733a189ce4734c446d90f18e
Redoy0 <redoyahamedrs@gmail.com>
2025-01-26 11:30:34 -0500

    First lab's Problem solved

--- reverse.cpp
+++ reverse.cpp
@@ -1,4 +1,31 @@
 #include<stdio.h>
 int main(){
-	printf("Hey Github\n");
+	int a[100],i,n;
+	printf("Enter Number of elements: ");
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+	}
+	printf("In same order: ");
+	for(i=0;i<n;i++){
+        printf("%d ",a[i]);
+	}
+	printf("\n");
+
+	for(i=0;i<n;i++){
+        for(int j=i+1;j<n-1;j++){
+            if(a[j]>a[i]){
+            int temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+            }
+        }
+	}
+
+	printf("In reverse sorting: ");
+	for(i=0;i<n;i++){
+        printf("%d ",a[i]);
+	}
+	printf("\n");
+
 }
